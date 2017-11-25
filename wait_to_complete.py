22c825e8226b673ed7e3916b6d0da0afbbb93feb
keegandsouza15 <keegan0415dsouza@gmail.com>
2017-11-25 10:42:00 -0800

    Waits for google compute operations to complete
    Python file that uses the google compute api's operations resource to tell whether an operation like, stopping, starting, or creating an instance has completed.

--- wait_to_complete.py
+++ wait_to_complete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import time
 
-# function takes in an operation and waits for it to complete.
+# function takes in an operation and waits for it to complete. 
 def wait (compute, project_id, zone, operation, message):
 	current_operation = compute.zoneOperations().get(project=project_id, zone=zone, operation=operation).execute()
 	status = current_operation['status']
